25232fef6a955ad95b5b16636c40e7a35827fe9a
m020202 <m020202@naver.com>
2024-08-02 21:58:57 +0900

    first init

---
+++ src/main/java/com/security1/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"", "/"})
+    public String index() {
+        // 머스테치 기본 폴더 src/main/resources/
+        // 뷰 리졸버 설정: templates (prefix), .mustache(suffix)
+        return "index"; // src/main/resources/templates/index.mustache
+    }
+}
